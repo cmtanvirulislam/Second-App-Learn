6e680b664dc6a816d622990291275bf62aa74b1f
Tanvir <engr.cmtanvir@gmail.com>
2021-01-29 21:05:26 +0600

    cross axis spacing added

--- lib/15.1 CardAndGridview.dart
+++ lib/15.1 CardAndGridview.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: GridView.count(
           mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
           crossAxisCount: 2,
           children: <Widget>[
             Container(
