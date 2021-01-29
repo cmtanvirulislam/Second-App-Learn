ee91a1e7639b56f5c4762108be22f7217827e0e0
Tanvir <engr.cmtanvir@gmail.com>
2021-01-29 21:03:08 +0600

    color added

---
+++ lib/15.1 CardAndGridview.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:secondapp/14%20LiquidSwipeInLiquidAnimation.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.green,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.brown,
+              ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.deepPurpleAccent,
+            ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.deepPurple,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
