f73745810bc2fc6d41dca5de71c73daf5048f446
Tanvir <engr.cmtanvir@gmail.com>
2021-02-03 15:29:55 +0600

    Gradient Color Added

---
+++ lib/23. Gradient color in appbar_container_gradient color.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePageTest()
+    );
+  }
+}
+
+
+class HomePageTest extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomePageTest> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepOrange,Colors.pinkAccent]
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
