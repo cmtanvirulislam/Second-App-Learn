dec5a69fdc0a8cd32806c53f1698cf1f1a612dc7
Tanvir <engr.cmtanvir@gmail.com>
2021-02-27 16:00:49 +0600

    Snackbar added

---
+++ lib/32. Snackbar in flutter.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:secondapp/17%20Pageview.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Builder(
+          builder: (BuildContext con){
+            return RaisedButton(
+              onPressed: () {
+                Scaffold.of(con).showSnackBar(SnackBar(
+                  content: Text("I am Tanvir. I like to work hard"),
+                  duration: Duration(seconds: 10),
+                  action: SnackBarAction(label: "ok", onPressed: (){}),
+                ));
+              },
+              child: Text("Click"),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
